6fb68e608fe383bbfc60b53969c3e44f6a9fee6f
RbBhuse <reshmabhuse7@gmail.com>
2023-12-26 20:47:35 +0530

    use of var JavaScript
    use of var JavaScript

---
+++ 0_1variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log(myFullName);// variable declaration
+
+myFullName= "Reshma Bhuse"; //initialize the value
+console.log(myFullName);
+
+myFullName="Reshma Prabhakar Bhuse";
+console.log("My full name is:", myFullName);
+
+var myLove="I love only JS";//declare and initialize variable
+console.log(myLove);
+
+var firstName="Reshma";
+var  lastname="Bhuse";
+var age="24";
+var address="Hinjewadi, Pune";
+var pinCode="411033";
+var gender="Female";
+var isMarried="yes";
+
+console.log("FirstName", firstName);
+console.log("LastName:", lastname);
+console.log("age",age);
+console.log("Address",address);
+console.log("PinCode", pinCode);
+console.log("Gender",gender);
+console.log("Marital status",isMarried); 
---
+++ helloWord.js
@@ -0,0 +1,16 @@
+console.log("Hello JS, I am Reshma");
+var num; //variable declartion
+console.log(num);
+
+var x="10"; //variaable Initalization
+console.log(x); 
+console.log("Value of X",x);
+
+var name="Reshma";
+console.log("First name",name)
+
+var nam="Reshma Bhuse";//update value
+console.log("My full name is", name);
+
+var lastName=null; //empty value
+console.log(lastName);
